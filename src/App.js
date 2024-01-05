384916e001093650045013c3e3dd095baa858cb4
harsh patel <harshpatel97762@gmail.com>
2024-01-05 17:12:32 +0530

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Main from './component/mainpage/Main';
+import Addpro from './component/add_product/Addpro';
+//import Footer from './component/footer/Footer';
+import Allproduct from './component/viewproductall/Allproduct';
+//import Header from './component/header/Header';
+import Login from './component/login/Login';
+//import Registration from './component/registration/Registration';
+import { Routes, Route } from 'react-router-dom';
+import Registration from './component/registration/Registration';
+import Privatecomponent from './component/Privatecomponent';
+import Updatepro from './component/updatepro/Updatepro';
+import Footer from './component/footer/Footer';
+import Header from './component/header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Header/>
+    <Routes>
+      <Route element={<Privatecomponent/>}>
+      <Route path="/" element={<Main />} />
+    
+      <Route path="/profile" element={<h1>profile</h1>} />
+      <Route path='/logout' element={<h1>logout</h1>}/>
+      <Route path='/addproduct' element={<Addpro/>}/>
+      <Route path='/updatepro/:id' element={<Updatepro/>}/>
+      <Route path='/allproduct' element={<Allproduct/>}/>
+      </Route>  
+      <Route path="/login" element={<Login />} />
+      <Route path="/registration" element={<Registration />} />
+    
+    </Routes>
+
+
+
     </div>
   );
 }
